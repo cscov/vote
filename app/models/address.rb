d49e41d9caeda0b36d18c9f2ccff0edc833b2751
cscov <carolyn.l.scoville@gmail.com>
2018-07-13 21:19:34 -0700

    Completed validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -17,19 +17,25 @@ class Address < ApplicationRecord
   # check street predirection is included in directions if present
   validates :street_predirection, inclusion: { in: %w('' N S E W NE SE NW SW),
                                                message: "must be a valid
-                                               cardinal direction" }
+                                               cardinal direction" },
+  if: :has_street_predirection?
 
   # check street post direction is included in directions if present
   validates :street_postdirection, inclusion: { in: %w('' N S E W NE SE NW SW),
-                                                message: "must be a valid 
-                                               cardinal direction" }
+                                                message: "must be a valid
+                                               cardinal direction" },
+  if: :has_street_postdirection?
 
   # unit number can't be blank
+  validates :unit_number, presence: true, if: :has_unit_type?
 
   # unit type either # or apt
+  validates :unit_type, inclusion: { in: %w('' # Apt), message: "must be
+    either #, or Apt" }, if: :has_unit_type?
 
   # city only letters, at least 2 char
-  validates :city, presence: true
+  validates :city, presence: true, format: { with: /[A-Za-z]{2,}/, message: "Must
+  be at least two letters" }
 
   # state included in states
   validates :state, inclusion: { in: %w(AL AK AZ AR CA CO CT DE DC FL
@@ -44,6 +50,8 @@ class Address < ApplicationRecord
   five digits" }
 
   # zip 4 must be 4 char, only numbers
+  validates :zip_4, format: { with: /[0-9]{4}/, message: "must be four
+  digits" }, if: :has_zip_4?
 
   def to_s
     # TODO: override the to_s method so that it prints out the address
@@ -53,5 +61,20 @@ class Address < ApplicationRecord
     "#{self.house_number} #{self.street_predirection} #{self.street_name} #{self.street_type} #{self.street_postdirection}#{self.unit_type} #{self.unit_number}, #{self.city}, #{self.state} #{self.zip_5}"
   end
 
+  def has_street_predirection?
+    self.street_predirection
+  end
+
+  def has_street_postdirection?
+    self.street_postdirection
+  end
+
+  def has_unit_type?
+    self.unit_type
+  end
+
+  def has_zip_4?
+    self.zip_4
+  end
 
 end
